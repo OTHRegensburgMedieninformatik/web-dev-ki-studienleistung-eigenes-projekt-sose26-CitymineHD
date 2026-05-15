dc618538cebc1726249475bfd6c73a226748d9bc
CitymineHD <lucas.pfeffer@gmx.de>
2026-05-15 17:46:13 +0200

    liite side quest: updated db encrypted data integration for personal data

--- models/user-store.js
+++ models/user-store.js
@@ -1,22 +1,49 @@
 const dataStore = require("./data-store.js"); 
 const dataStoreClient = dataStore.getDataStore(); 
 const logger = require("../utils/logger.js"); 
-const { log } = require("winston");
+const crypto = require("../models/crypto.js");
+const cryptoLib = require("crypto");
  
 const userStore = { 
     async addUser(user) { 
-        const query_account = 'INSERT INTO project.account (username, password) VALUES($1, $2) RETURNING id';
+        const iv = cryptoLib.randomBytes(16);
+
+        const query_account = 'INSERT INTO project.account (username, password, salt) VALUES($1, $2, $3) RETURNING id';
         const query_role = 'INSERT INTO project.role (id, role) VALUES($1, $2)';
-        const query_member_data = 'INSERT INTO project.member_data VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)';
+        const query_member_data = 'INSERT INTO project.member_data VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)';
         const query_apply_status = 'INSERT INTO project.apply_status (user_id, status) VALUES($1, $2)';
-        const values_account = [user.firstname + "." + user.lastname, user.postcode];
+
+        const {hash, salt} = crypto.hashPassword(user.postcode);
+
+        const values_account = [user.firstname + "." + user.lastname, hash, salt];
+
         try { 
             let acc = await dataStoreClient.query(query_account, values_account);
 
             const values_role = [acc.rows[0].id, 'member'];
             await dataStoreClient.query(query_role, values_role);
             
-            const values_member_data = [acc.rows[0].id, user.lastname, user.firstname, user.birthday, user.address, user.postcode, user.city, user.phone, user.handy, user.mail, user.bank_firstname, user.lastname, user.bank_address, user.iban, user.bic, user.institut, user.department, user.privacy, user.newsletter];
+            const values_member_data = [
+                acc.rows[0].id, 
+                user.lastname, 
+                user.firstname, 
+                crypto.encrypt(user.birthday, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.address, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.postcode, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.city, iv, process.env.CRYPTO_KEY),
+                user.phone, 
+                user.handy, 
+                user.mail, 
+                crypto.encrypt(user.bank_firstname, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.bank_lastname, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.bank_address, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.iban, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.bic, iv, process.env.CRYPTO_KEY),
+                crypto.encrypt(user.institut, iv, process.env.CRYPTO_KEY),
+                user.department, 
+                user.privacy, 
+                user.newsletter,
+                iv.toString('hex')];
             await dataStoreClient.query(query_member_data, values_member_data);
 
             const values_apply_status = [acc.rows[0].id, 0];
@@ -27,12 +54,16 @@ const userStore = {
     },
 
     async authenticateUser(username, password) { 
-        const query = 'SELECT * FROM project.account join project.role on project.account.id = project.role.id WHERE username=$1 AND password=$2'; 
-        const values = [username, password];
+        const query = 'SELECT * FROM project.account join project.role on project.account.id = project.role.id WHERE username=$1'; 
+        const values = [username];
         try { 
-            let dbRes = await dataStoreClient.query(query, values); 
+            let dbRes = await dataStoreClient.query(query, values);
+            console.log(dbRes.rows[0]);
             if (dbRes.rows[0] !== undefined) { 
-                return {id: dbRes.rows[0].id, username: username, role: dbRes.rows[0].role}; 
+                if (crypto.verifyPassword(password, dbRes.rows[0].password, dbRes.rows[0].salt)) {
+                    return {id: dbRes.rows[0].id, username: dbRes.rows[0].username, role: dbRes.rows[0].role};
+                }
+                return undefined;
             } else { 
                 return undefined; 
             } 
@@ -43,13 +74,13 @@ const userStore = {
 
     async getUserById(id) { 
         logger.info(`Getting user ${id}`); 
-        const query = 'SELECT * FROM project.member_data WHERE id=$1';
+        const query = 'SELECT * FROM project.member_data join project.apply_status on project.member_data.id = project.apply_status.user_id WHERE project.member_data.id=$1';
         const values = [id]; 
         try { 
             let dbRes = await dataStoreClient.query(query, values); 
             logger.info(`Getting user ${dbRes.rows[0].username}`); 
             if (dbRes.rows[0] !== undefined) { 
-                return {id: dbRes.rows[0].id, lastName: dbRes.rows[0].lastname, firstName: dbRes.rows[0].firstname, birthday: dbRes.rows[0].birthday, address: dbRes.rows[0].address, postcode: dbRes.rows[0].postcode, city: dbRes.rows[0].city, phone: dbRes.rows[0].phone, handy: dbRes.rows[0].handy, mail: dbRes.rows[0].mail, bankFirstName: dbRes.rows[0].bank_firstname, bankLastName: dbRes.rows[0].bank_lastname, bankAddress: dbRes.rows[0].bank_address, iban: dbRes.rows[0].iban, bic: dbRes.rows[0].bic, institut: dbRes.rows[0].institut, department: dbRes.rows[0].department, privacy: dbRes.rows[0].privacy, newsletter: dbRes.rows[0].newsletter}; 
+                return {id: dbRes.rows[0].id, lastName: dbRes.rows[0].lastname, firstName: dbRes.rows[0].firstname, birthday: crypto.decrypt(dbRes.rows[0].birthday, dbRes.rows[0].iv, process.env.CRYPTO_KEY), address: crypto.decrypt(dbRes.rows[0].address, dbRes.rows[0].iv, process.env.CRYPTO_KEY), postcode: crypto.decrypt(dbRes.rows[0].postcode, dbRes.rows[0].iv, process.env.CRYPTO_KEY), city: crypto.decrypt(dbRes.rows[0].city, dbRes.rows[0].iv, process.env.CRYPTO_KEY), phone: dbRes.rows[0].phone, handy: dbRes.rows[0].handy, mail: dbRes.rows[0].mail, bankFirstName: crypto.decrypt(dbRes.rows[0].bank_firstname, dbRes.rows[0].iv, process.env.CRYPTO_KEY), bankLastName: crypto.decrypt(dbRes.rows[0].bank_lastname, dbRes.rows[0].iv, process.env.CRYPTO_KEY), bankAddress: crypto.decrypt(dbRes.rows[0].bank_address, dbRes.rows[0].iv, process.env.CRYPTO_KEY), iban: crypto.decrypt(dbRes.rows[0].iban, dbRes.rows[0].iv, process.env.CRYPTO_KEY), bic: crypto.decrypt(dbRes.rows[0].bic, dbRes.rows[0].iv, process.env.CRYPTO_KEY), institut: crypto.decrypt(dbRes.rows[0].institut, dbRes.rows[0].iv, process.env.CRYPTO_KEY), department: dbRes.rows[0].department, privacy: dbRes.rows[0].privacy, newsletter: dbRes.rows[0].newsletter, applyStatus: dbRes.rows[0].status}; 
             } else { 
                 return undefined; 
             } 
@@ -60,12 +91,12 @@ const userStore = {
 
     async getUserApplyStatus(id) {
         logger.info(`Getting user-apply-status ${id}`);
-        const query = 'SELECT * FROM project.apply_status WHERE user_id=$1'; 
-        const values = [id]; 
-        try { 
+        const query = 'SELECT status FROM project.apply_status WHERE user_id=$1'; 
+        const values = [id];
+        try {
             let dbRes = await dataStoreClient.query(query, values); 
             if (dbRes.rows[0] !== undefined) { 
-                return {id: dbRes.rows[0].id, status: dbRes.rows[0].status}; 
+                return {status: dbRes.rows[0].status}; 
             } else { 
                 return undefined; 
             } 
@@ -76,7 +107,7 @@ const userStore = {
 
     async getAllUserApplys() {
         logger.info('Getting all user-new-applys');
-        const query = 'SELECT * FROM project.apply_status join project.member_data on user_id = id where status=0';
+        const query = 'SELECT user_id, project.member_data.firstname, project.member_data.lastname FROM project.apply_status join project.member_data on user_id = id where status=0';
 
         try {
             let dbRes = await dataStoreClient.query(query); 
@@ -92,7 +123,7 @@ const userStore = {
 
     async getAllUsers() {
         logger.info('Getting all Users');
-        const query = 'SELECT * FROM project.apply_status join project.member_data on user_id = id where status=1';
+        const query = 'SELECT user_id, project.member_data.firstname, project.member_data.lastname FROM project.apply_status join project.member_data on user_id = id where status=1';
 
         try {
             let dbRes = await dataStoreClient.query(query); 
@@ -114,19 +145,26 @@ const userStore = {
         try {
             await dataStoreClient.query(query, values)
         } catch (e) { 
-            console.log("Error getting apply-status", e); 
+            console.log("Error updating user status", e); 
         }
     },
 
     async updateUserProfile(userID, updatedData) {
         logger.info('Updating User ${userId}')
-        const query = 'update project.member_data set lastname=$1, firstname=$2, birthday=$3, address=$4, postcode=$5, city=$6, phone=$7, handy=$8, mail=$9 where id=$10';
-        const values = [updatedData.lastName, updatedData.firstName, updatedData.birthday, updatedData.address, updatedData.postcode, updatedData.city, updatedData.phone, updatedData.handy, updatedData.mail, userID];
 
+        const query_iv = 'SELECT iv FROM project.member_data WHERE id=$1';
+        const values_iv = [userID];
+        
         try {
-            await dataStoreClient.query(query, values)
+
+            const dbRes = await dataStoreClient.query(query_iv, values_iv);
+
+            const query_update = 'update project.member_data set lastname=$1, firstname=$2, birthday=$3, address=$4, postcode=$5, city=$6, phone=$7, handy=$8, mail=$9 where id=$10';
+            const values_update = [updatedData.lastName, updatedData.firstName, crypto.encrypt(updatedData.birthday, Buffer.from(dbRes.rows[0].iv, 'hex'), process.env.CRYPTO_KEY), crypto.encrypt(updatedData.address, Buffer.from(dbRes.rows[0].iv, 'hex'), process.env.CRYPTO_KEY), crypto.encrypt(updatedData.postcode, Buffer.from(dbRes.rows[0].iv, 'hex'), process.env.CRYPTO_KEY), crypto.encrypt(updatedData.city, Buffer.from(dbRes.rows[0].iv, 'hex'), process.env.CRYPTO_KEY), updatedData.phone, updatedData.handy, updatedData.mail, userID];
+           
+            await dataStoreClient.query(query_update, values_update);
         } catch (e) {
-            console.log("Error getting apply-status", e); 
+            console.log("Error updating user profile", e); 
         }
     },
 
