7123ec0bcaa1b967f1285da35a97828e98e2ec1f
CitymineHD <lucas.pfeffer@gmx.de>
2026-05-18 23:02:29 +0200

    filled database with data and took some adjustment in the content views leading/contact/soccer/coaches

--- models/contact-store.js
+++ models/contact-store.js
@@ -4,7 +4,7 @@ const logger = require("../utils/logger.js");
 
 const contactStore = { 
     async getContactsByPositionGroup(positionGroup) {
-        const query = 'SELECT * FROM project.member_data join project.personal on using(id) where position_group = $1'; 
+        const query = 'SELECT * FROM project.member_data join project.personal on project.member_data.id = project.personal.id where position_group = $1'; 
         try { 
         let result = await dataStoreClient.query(query, [positionGroup]); 
         return result.rows; 
